46e7dd3db705dcee6c4fca9867a4ad51aa09401d
cas5ga <42973719+cas5ga@users.noreply.github.com>
2019-10-31 21:07:30 -0400

    Version 3

--- ConnectFour.py
+++ ConnectFour.py
@@ -1,26 +1,42 @@
+# Start createBoard
 def createBoard():
     # Creates th initial board which is a matrix of 0's
-    board = [[0,0,1,0,2,0,0],
-             [0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0]]
+    board = [[0,0,2,0,0,0,0],
+             [0,0,1,0,0,0,0],
+             [0,0,2,0,0,0,0],
+             [0,0,1,0,0,0,0],
+             [0,0,2,0,0,0,0],
+             [0,0,1,1,1,0,0]]
     return board
+# End createBoard
 
-def updateBoard(uBoard, column):
+# Start updateBoard
+def updateBoard(uBoard, column, turn):
     # Takes the user's move and updates the board
-    for row in range(1):
-        rowCheck = row - 1
-        while uBoard[rowCheck+1][column] is 0 and rowCheck+1 is not 6:
-            rowCheck += 1
+    saveRow = 0
+    for row in range(6):
+        if uBoard[row][column] is not 0:
+            saveRow = row - 1
+            break
+        elif row is 5:# If a column is empty, sets saveRow to row 
+            saveRow = row
+            break
+    
+    if(turn is 1):
+        uBoard[saveRow][column] = turn
+    
+    if(turn is 2):
+        uBoard[saveRow][column] = turn
     
     # Prints the updated board
+    print(' 0  1  2  3  4  5  6')
     for row in uBoard:
         print(row)
-                
-# Player one's turn
+# End updateBoard
+            
+# Start playerOne
 def playerOne(board):
+    turn = 1 # Needed to tell board it's player one's move
     # Gets user input and converts it to integer
     move = int(input("Player one, please select a column to play in.\n"))
     while move < 0 or move > 6:# Checks that user has selected an existing column
@@ -28,18 +44,21 @@ def playerOne(board):
     while board[0][move] is not 0:# Checks if a column is full 
         move = int(input('That column is full, please select a different column.\n'))
         move = int(move)
-    updateBoard(board, move)# Sends move to the baord to be updated
+    updateBoard(board, move, turn)# Sends move to the baord to be updated
+# End playerOne
 
-# Player tow's turn
+# Start playerTwo
 def playerTwo(board):
+    turn = 2 # Need to tell server it's player two's turn
     # Gets user input and converts it to integer
     move = int(input("Player two, please select a column to play in.\n"))
     while move < 0 or move > 6: # Checks that user has selected an existing column
         move = int(input('Column not allowed, please select an appropriate column.\n'))
     while board[0][move] is not 0: # Checks if a column is full 
         move = int(input('That column is full, please select a different column.\n'))
-    updateBoard(board, move)# Sends move to the baord to be updated
+    updateBoard(board, move, turn)# Sends move to the baord to be updated
 
+# Main
 # Function call to get the baord
 myBoard = createBoard()
 
