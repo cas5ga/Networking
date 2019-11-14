88a3712bbde1fae33a13fd98871de5dfd5f1f2b1
cas5ga <42973719+cas5ga@users.noreply.github.com>
2019-11-13 22:13:48 -0500

    Add files via upload

---
+++ connectFour.py
@@ -0,0 +1,123 @@
+# Start createBoard
+def createBoard():
+    # Creates th initial board which is a matrix of 0's
+    board = [[0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0]]
+    return board
+# End createBoard
+
+# Start winCheck
+def winCheck(myBoard, t, checkWin):
+    # Checks for vertical win
+    for row in range(3):
+        for column in range(7):
+            if myBoard[row][column] == t and myBoard[row+1][column] == t:
+                if myBoard[row+2][column] == t and myBoard[row+3][column] == t:
+                    checkWin = True
+                    break
+                
+    # Check for horizontal win                
+    for row in range(6):
+        for column in range(4):
+            if myBoard[row][column] == t and myBoard[row][column+1] == t:
+                if myBoard[row][column+2] == t and myBoard[row][column+3] == t:
+                    checkWin = True
+                    break
+                
+    # Check for up-right diagonal win
+    for row in range(3, 6):
+        for column in range(3):
+            if myBoard[row][column] == t and myBoard[row-1][column+1] == t:
+                if myBoard[row-2][column+2] == t and myBoard[row-3][column+3] == t:
+                    checkWin = True
+                    break
+                
+    # Check fpr down-right diagonal win
+    for row in range(3):
+        for column in range(4):
+            if myBoard[row][column] == t and myBoard[row+1][column+1] == t:
+                if myBoard[row+2][column+2] == t and myBoard[row+3][column+3] == t:
+                    checkWin = True
+                    break
+                
+    return checkWin
+    
+# Start updateBoard
+def updateBoard(uBoard, column, turn, win):
+    # Takes the user's move and updates the board
+    saveRow = 0
+    for row in range(6):
+        if uBoard[row][column] is not 0:
+            saveRow = row - 1
+            break
+        elif row is 5:# If a column is empty, sets saveRow to row 
+            saveRow = row
+            break
+    
+    if(turn is 1):
+        uBoard[saveRow][column] = turn
+    
+    if(turn is 2):
+        uBoard[saveRow][column] = turn
+    
+    win = winCheck(uBoard, turn, win)
+    
+    # Prints the updated board
+    print(' 0  1  2  3  4  5  6')
+    for row in uBoard:
+        print(row)
+    
+    return win
+# End updateBoard
+            
+# Start playerOne
+def playerOne(board, isWin):
+    turn = 1 # Needed to tell board it's player one's move
+    # Gets user input and converts it to integer
+    move = int(input("Player one, please select a column to play in.\n"))
+    while move < 0 or move > 6:# Checks that user has selected an existing column
+        move = int(input('Column not allowed, please select an appropriate column.\n'))
+    while board[0][move] is not 0:# Checks if a column is full 
+        move = int(input('That column is full, please select a different column.\n'))
+        move = int(move)
+    isWin = updateBoard(board, move, turn, isWin)# Sends move to the baord to be updated
+    if isWin is True:
+        print("Player One wins!")
+    return isWin
+# End playerOne
+
+# Start playerTwo
+def playerTwo(board, isWin):
+    turn = 2 # Need to tell server it's player two's turn
+    # Gets user input and converts it to integer
+    move = int(input("Player two, please select a column to play in.\n"))
+    while move < 0 or move > 6: # Checks that user has selected an existing column
+        move = int(input('Column not allowed, please select an appropriate column.\n'))
+    while board[0][move] is not 0: # Checks if a column is full 
+        move = int(input('That column is full, please select a different column.\n'))
+    isWin = updateBoard(board, move, turn, isWin)# Sends move to the baord to be updated
+    if isWin is True:
+        print("Player Two wins!")
+    return isWin
+
+# Main
+# Function call to get the baord
+myBoard = createBoard()
+
+win = False
+# Labels for columns
+print(' 0  1  2  3  4  5  6')
+
+# Prints the initial board
+for row in myBoard:
+    print(row)
+
+# Continues playing the game until a winner is found
+while win is False:
+    win = playerOne(myBoard, win) # Calls for player one's turn
+    if win is False: 
+        win = playerTwo(myBoard, win) # Calls for player two's turn
